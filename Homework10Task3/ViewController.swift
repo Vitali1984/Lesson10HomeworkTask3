e85c130ce5bc39b82b6ede2baed68cecb1d098b8
Vitali1984 <vitali.kuchinski1984@gmail.com>
2022-01-27 11:29:31 +0300

    Add navigation controller

--- Homework10Task3/ViewController.swift
+++ Homework10Task3/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onStartWCButton(_ sender: Any) {
+        let vc = UIStoryboard(name: "GameViewController", bundle: Bundle.main).instantiateInitialViewController() as! GameViewController
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    @IBAction func StartWSButton(_ sender: Any) {
+        let vc = UIStoryboard(name: "GameCodeViewController", bundle: Bundle.main).instantiateInitialViewController() as! GameCodeViewController
+        navigationController?.pushViewController(vc, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
