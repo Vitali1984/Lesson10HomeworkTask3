0887ac953e0b7d93d366da5d1f4c342ee4db54bf
Vitali1984 <vitali.kuchinski1984@gmail.com>
2022-01-27 11:10:00 +0300

    Add project

---
+++ Homework10Task3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework10Task3
+//
+//  Created by Виталий on 27.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
