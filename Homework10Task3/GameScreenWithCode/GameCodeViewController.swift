1c77d795b430a4fe4e68a0cd04e1c48639aa8809
Vitali1984 <vitali.kuchinski1984@gmail.com>
2022-01-27 12:43:18 +0300

    game with code

--- Homework10Task3/GameScreenWithCode/GameCodeViewController.swift
+++ Homework10Task3/GameScreenWithCode/GameCodeViewController.swift
@@ -8,13 +8,53 @@
 import UIKit
 
 class GameCodeViewController: UIViewController {
-
+    
+    private var labelMove: UILabel!
+    
+    // MARK: - ViewControllerLifeCycle
+    // MARK: -
+    override func loadView() {
+        let customView = UIView(frame: UIScreen.main.bounds)
+        
+        labelMove = UILabel()
+        labelMove.backgroundColor = .systemRed
+        customView.addSubview(labelMove)
+        
+        view = customView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(onTapAction))
+        view.addGestureRecognizer(tap)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(onPanAction))
+        view.addGestureRecognizer(pan)
 
         // Do any additional setup after loading the view.
     }
     
+    // MARK: - Layout
+    // MARK: -
+    
+//    override func viewWillLayoutSubviews() {
+//    }
+
+    // MARK: - ActionsRecognizer
+    // MARK: -
+    @objc func onTapAction(gesture: UITapGestureRecognizer) {
+        labelMove.frame.origin = gesture.location(in: view)
+        labelMove.frame.size = CGSize(width: 100, height: 100)
+        labelMove.layer.cornerRadius = 0.5 * labelMove.bounds.size.width
+        labelMove.clipsToBounds = true
+        
+    }
+    @objc func onPanAction(gesture: UIPanGestureRecognizer) {
+        labelMove.frame.origin = gesture.location(in: view)
+    }
+
 
     /*
     // MARK: - Navigation
